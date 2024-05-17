ab9fbbd259e16417cd6907d08f982dcabc19ca4e
realcraft3333 <162047553+realcraft3333@users.noreply.github.com>
2024-05-17 20:26:33 +0000

    Mundo guardado 2024-05-17

--- servidor.py
+++ servidor.py
@@ -6,7 +6,6 @@
 # Custom version
 # mohist / catserver   Install forge   first
 # purpur               Install fabric  first
-# snapshot             Install vanilla first
 
 # Ngrok region
 # Code           Place
